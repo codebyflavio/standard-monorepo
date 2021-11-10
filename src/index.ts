e58e93cf66bf36ebdb8759baeac6c5ba93a5b282
Flávio Carvalho <hey@flavio.uk>
2021-11-10 11:41:15 +0000

    feat: expose get changed packages

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { run } from '@oclif/command'
 export { default as getAllPackages } from './core/allPackages'
+export { default as getChangedFiles } from './core/changedFiles'
 export { Graph } from './core/graph'
